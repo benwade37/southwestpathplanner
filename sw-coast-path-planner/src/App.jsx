5c206e9ce26188a9921db4dc4935874d529bf49d
Ben <benwade37@me.com>
2026-06-29 15:56:35 +0100

    Create initial React app

---
+++ sw-coast-path-planner/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div>
+      <h1>South West Coast Path Planner</h1>
+
+<p>Track your progress along the South West Coast Path.</p>
+
+<ul>
+  <li>Welcombe Mouth → Hartland Quay</li>
+  <li>Hartland Quay → Clovelly</li>
+  <li>Portreath → St Agnes</li>
+</ul>
+    </div>
+  )
+}
+
+export default App
